3201967193ecb4c1228454f1f42c70ff5499a709
jrbnewman <jrbnewman@ufl.edu>
2018-06-08 16:40:29 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,63 @@
+# Setup event_analysis
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here,'README.md'), encoding='utf-8') as f:
+    long_description=f.read()
+
+setup(
+    name='event_analysis',
+
+    # Version
+    version="1.0.0",
+
+    description="Event Analysis python workflow",
+    long_description=long_description,
+
+    # URL to github
+    url="https://github.com/McIntyre-Lab/events.git",
+
+    # Author details
+    author="Jeremy R. B. Newman",
+    author_email="jrbnewman@ufl.edu",
+
+    license="BSD",
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Geneticists',
+        'Topic :: Software Development :: Libraries :: Python Packages',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language:: Python:: 3:: Only',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # What does your project relate to?
+    keywords='genetics',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed.
+    install_requires=['argparse','io','openpyxl','operator','csv','datetime','gffutils','glob','itertools','logging','numpy','os','pandas','pybedtools','sqlite3','sys']
+)
